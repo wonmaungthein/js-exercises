f387af1634cf1aeae4d31e25cb19a6cd25ab2c81
Won Maung Thein <wonmaungthein@gmail.com>
2018-03-01 21:50:51 +0000

    done some exercises

--- week-2/E-conditionals/exercise.js
+++ week-2/E-conditionals/exercise.js
@@ -1,24 +1,125 @@
-/*
-  Conditionals
-  ---------------------------------
-  Add an if statement to only show the name if the role is student
-*/
+// /*
+//   Conditionals
+//   ---------------------------------
+//   Add an if statement to only show the name if the role is student
+// */
 
 
-var name = "Daniel";
-var role = "mentor";
+// var name = "Daniel";
+// var role = "mentor";
 
-var name2 = "Harun";
-var role2 = "student";
-
-
-console.log("Hi, My name is " + name);
-console.log("Hi, My name is " + name2);
+// var name2 = "Harun";
+// var role2 = "student";
 
 
+// // console.log("Hi, My name is " + name);
+// // console.log("Hi, My name is " + name2);
 
-/* 
-EXPECTED RESULT
----------------
-My name is Harun
-*/
+// // if (role2 === 'student'){
+// //   console.log("Hi, My name is " + name2);}
+// //   else  {
+// //     console.log("Hi, My name is " + name);
+// // }
+
+// if (role2 === 'student' ? console.log("Hi, My name is " + name2): console.log("Hi, My name is " + name));
+
+
+// /* 
+// EXPECTED RESULT
+// ---------------
+// My name is Harun
+// */
+
+
+// const name = 'Hervin';
+
+// for (var i = 0; i < 10; i++){
+//   console.log (`My name is ${name}`);
+// };
+
+
+
+// const today = 'Satu';
+
+// today === 'Saturday' ? console.log('Let\'s go to CYF') : console.log('Stay home');
+
+
+// 13 % 2 === 1; // 13 divided by 2 leaves a remainder of 1
+// 100 % 10 === 0; // 100 divided by 10 leaves a remainder of 0
+// Now improve your code so that it prints out if the number is positive or negative, as well as even or odd. For example, for -5 it will print 'negative odd', while for 4 it will print 'positive even'.
+
+// Hint: You can use the > or < signs. Check the list of comparison operators provided before the Exercise to learn what they do.
+
+// If you finish early, think about how you would test your code works correctly?
+
+// const number = 4;
+
+// if (number === -5){
+//   console.log ('Negative odd')
+// } else {
+//   console.log ('positive even')
+// };
+
+
+// function sum (x,y){
+//   return x + y
+// }
+// console.log(sum(2, 3));
+
+// const age = 26
+
+// function buildAgeMessage (age){
+//   return ` I am ${age} years old`
+// }
+
+// console.log (buildAgeMessage(age));
+
+// var mentorAge = 25;
+
+// function thisOver(age){
+//  return "this is " + age + "year old"
+// }
+// console.log(thisOver(26));
+
+
+// for (var i = 1; i <= 15; i++) {
+//   if (i === 3) {
+//     console.log(`I am super ${i}`)
+//   } else if (i=== 9)  {
+//     console.log(`This is number dupper ${i}`)
+//   } else {
+//     console.log (`I am new man`);
+//   }
+// };
+
+// var number = -1;
+
+// function isnegativeOrPositive(number){
+//   if (number >= 0){
+//     return "Positive"
+//   } else {
+//     return  "Negative"
+//   }
+// }
+
+
+// console.log(isnegativeOrPositive(number));
+
+
+
+// const mentors = [];
+// mentors.push('Won','Harvin', 'Nasir');
+// console.log(mentors[1]);
+
+
+// for (var i = 1; i <= 100; i++ ){
+//   if (i % 2 === 0){
+//     console.log (i);
+//   } 
+// }
+
+
+// Changing the name from Aray.
+const friends = ['Harvin', 'Nasir', 'Won'];
+friends.pop()
+console.log (friends);
