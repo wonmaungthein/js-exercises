32a9dd0ac9f85aa98a1dd3194814b272dd3f5a7e
Won Maung Thein <wonmaungthein@gmail.com>
2018-03-15 21:53:43 +0000

    done homework exercise G

--- week-1/G-strings-methods/exercise.js
+++ week-1/G-strings-methods/exercise.js
@@ -1,3 +1,10 @@
 // Start by creating a variable `message`
+const name = "Won Maung Thein";
+const nameLength = name.length;
+const message = `My name is ${name} and my name is ${nameLength} characters long.`;
 
+console.log(name);
+console.log(nameLength);
 console.log(message);
+
+// DONE HOMEWORK
--- week-1/G-strings-methods/exercise2.js
+++ week-1/G-strings-methods/exercise2.js
@@ -1,3 +1,14 @@
 var name = " Daniel  ";
+const formatted = name.trim();
+console.log(formatted);
 
-console.log(message);
+const myName = "  Won maung Thein        ";
+const myNameLength = myName.length;
+console.log(myNameLength);
+
+const spaceTrimmed = myName.trim();
+console.log(spaceTrimmed);
+
+console.log(spaceTrimmed.length);
+
+// DONE HOMEWORK
