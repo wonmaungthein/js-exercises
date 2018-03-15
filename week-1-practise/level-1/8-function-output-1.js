0e3698e504173083aa5cc78420c7423640610672
Won Maung Thein <wonmaungthein@gmail.com>
2018-03-15 17:14:41 +0000

    done exercise 8

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -7,6 +7,13 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do?
+// This function run, took random num and multiply with number 10. 
+
 // - How many parameters does the function take?
+// It didn't take any number.
+
 // - What is the function name?
+// getNumber
+
 // - Where is the function called? with what parameters?
+// console.log(result) no parameters;
