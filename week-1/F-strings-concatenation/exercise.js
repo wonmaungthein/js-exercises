09618406fa851dd8e39ac17966b9628c029d5708
Won Maung Thein <wonmaungthein@gmail.com>
2018-03-15 21:42:31 +0000

    done exercise

--- week-1/F-strings-concatenation/exercise.js
+++ week-1/F-strings-concatenation/exercise.js
@@ -1,3 +1,6 @@
 // Start by creating a variable `message`
-
+const name = "Won Maung Thein";
+const sentence = "Hello everyone, Let me introduce myself.";
+const hobby = "Coding";
+const message = `${sentence} I am ${name} and I love ${hobby}.`;
 console.log(message);
