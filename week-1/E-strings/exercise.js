120f12806587055f1b46f5dcc2ea5229a9725eff
Won Maung Thein <wonmaungthein@gmail.com>
2018-03-15 21:31:35 +0000

    done exercise E

--- week-1/E-strings/exercise.js
+++ week-1/E-strings/exercise.js
@@ -1,3 +1,27 @@
 // Start by creating a variable `message`
 
+const message = `This is string and this is the value of message.`;
 console.log(message);
+
+const messageType = typeof message;
+console.log(messageType);
+
+const integer = 2;
+console.log(integer);
+
+const integerType = typeof integer;
+console.log(integerType);
+
+const myArray = [1, 2, 4, 6];
+console.log(myArray);
+
+const isWhatType = typeof myArray;
+console.log(isWhatType);
+
+const correct = true;
+console.log(correct);
+
+const correctType = typeof correct;
+console.log(correctType);
+
+// DONE HOMEWORK
