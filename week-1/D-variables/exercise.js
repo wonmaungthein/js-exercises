866b382179f039f1b2e27391be840fa6355a4b1f
Won Maung Thein <wonmaungthein@gmail.com>
2018-03-15 21:22:25 +0000

    did exercises

--- week-1/D-variables/exercise.js
+++ week-1/D-variables/exercise.js
@@ -1,3 +1,13 @@
 // Start by creating a variable `greeting`
 
+const greeting = `Hello world, welcome to coding world.!!`;
 console.log(greeting);
+console.log(greeting);
+console.log(greeting);
+
+const age = 33;
+
+const sentence = (greeting, age) =>
+  `${greeting} My name is Won Maung Thein, I am ${age} years old.`;
+
+console.log(sentence(greeting, age));
