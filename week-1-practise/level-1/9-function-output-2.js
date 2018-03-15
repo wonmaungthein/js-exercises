6f2a062bedb1cc7c09f69f4ff6b722890ea0919d
Won Maung Thein <wonmaungthein@gmail.com>
2018-03-15 17:18:39 +0000

    done exe 9

--- week-1-practise/level-1/9-function-output-2.js
+++ week-1-practise/level-1/9-function-output-2.js
@@ -7,8 +7,18 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do? Run it and see, or Google some of the methods used ot understand them.
+// It took 2 parameters, it concat first parameter with second parameter.
+
 // - How many parameters does the function take?
+// 2
 // - What is the function name?
+// s
+
 // - Where is the function called? with what parameters?
+// console.log(result);
+
 // - What does the function return?
+// first parame concat second para
+
 // - How can this function be improved?
+// by giving a good function name eg. concatParam
