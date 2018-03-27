b7907e01880b4295ce8f2898c9a6b4f1f274dae7
Won Maung Thein <wonmaungthein@gmail.com>
2018-03-27 22:30:44 +0100

    fetched api and displayed in html

--- week-5/A-fetch/exercise.js
+++ week-5/A-fetch/exercise.js
@@ -17,10 +17,13 @@ Open index.html in your browser. Every time you refresh the page,
 a different greeting should be displayed in the box.
 */
 
-fetch('*** Write the API address here ***')
-    .then(function(response) {
-        return response.text();
-    })
-    .then(function(greeting) {
-        // Write the code to display the greeting text here
-    });
+const greetingSpace = document.querySelector("#greeting-text");
+
+fetch("https://codeyourfuture.herokuapp.com/api/greetings")
+  .then(function(response) {
+    return response.text();
+  })
+
+  .then(function(greeting) {
+    greetingSpace.innerHTML = greeting;
+  });
