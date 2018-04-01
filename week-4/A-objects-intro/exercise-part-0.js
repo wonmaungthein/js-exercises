fed41f91b6aa287c19bac3e4f7b66511e82ceea4
Won Maung Thein <wonmaungthein@gmail.com>
2018-04-01 13:16:48 +0100

    done exercise 0

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,22 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+
+const myLaptop = {
+  type: "Laptop",
+  model: "MacBook-pro",
+  year: 2017,
+  made: "Apple Co",
+  battery: "ten hours long",
+  key: "Butterfly key",
+  display: "Retina"
+};
+
+console.log(myLaptop);
+console.log(myLaptop.type);
+console.log(myLaptop.model);
+console.log(myLaptop.year);
+console.log(myLaptop.made);
+console.log(myLaptop.battery);
+console.log(myLaptop.key);
+console.log(myLaptop.display);
