568aff6551696cc90eac667bf3155b4b0cd1df87
Won Maung Thein <wonmaungthein@gmail.com>
2018-04-01 13:39:20 +0100

    done exercise 1

--- week-4/A-objects-intro/exercise-part-1.js
+++ week-4/A-objects-intro/exercise-part-1.js
@@ -7,3 +7,45 @@ Assign each of them to a separate variable
 
 */
 
+var myFriend = {
+  firstName: "John",
+  lastName: "Thein",
+  gender: "Male",
+  hobbies: "Swimming",
+  occupation: "Software engineer",
+  residence: "UK",
+  attitude: "Positive"
+};
+
+var favouriteFood = {
+  type: "vegetable",
+  method: "fried",
+  freshness: "organic",
+  cost: "cheap"
+};
+
+var clothes = {
+  season: "summer",
+  quality: "fair",
+  cost: "reasonably cheap",
+  made: "Any kind",
+  fashion: "light"
+};
+
+var inspiredPeople = {
+  gender: "any",
+  attitude: "positive",
+  race: "any",
+  personality: "driven",
+  skills: ["Public speaking", "Time Management"],
+  goals: "Helping out"
+};
+
+var favouriteCar = {
+  made: "USA",
+  brand: "Tesla",
+  color: "White",
+  capacity: "seven",
+  gas: false,
+  energyType: "electric"
+};
